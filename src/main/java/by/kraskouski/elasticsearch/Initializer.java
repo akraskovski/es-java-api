92a8b3b0ffbb2284cd9d8218a0b691599504f584
akraskovski <artem.kraskovski@gmail.com>
2017-12-25 18:08:50 +0300

    initial structure

---
+++ src/main/java/by/kraskouski/elasticsearch/Initializer.java
@@ -0,0 +1,12 @@
+package by.kraskouski.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Initializer {
+
+    public static void main(final String... args) {
+        SpringApplication.run(Initializer.class, args);
+    }
+}
